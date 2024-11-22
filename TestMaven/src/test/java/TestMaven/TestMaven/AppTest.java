e94cf6cd11c491fa79240d317d69057b782a7fa4
Raja <raja.k@congruentindia.com>
2024-11-22 15:25:23 +0530

    First Commit

---
+++ TestMaven/src/test/java/TestMaven/TestMaven/AppTest.java
@@ -0,0 +1,15 @@
+package TestMaven.TestMaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    
+	@Test
+    public void testApp()
+    {
+	System.out.println("Test Maven");
+    }
+}
